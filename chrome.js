fd78d68446163bedfff466a38c49ce6d92aa73de
aperez <aperez@falconsdigital.com>
2022-04-01 15:46:42 -0400

    fix bugs

---
+++ chrome.js
@@ -0,0 +1,16 @@
+import falconsCentralURL from "./assets/FalconsCentral.glb";
+
+const astronaut = "https://modelviewer.dev/shared-assets/models/Astronaut.glb";
+
+let toggleModel = false;
+
+const btn = document.getElementById("switch-model");
+
+btn.onclick = () => {
+    toggleModel = !toggleModel;
+    btn.classList.toggle("falcon");
+    btn.innerHTML = `${toggleModel ? "Astronaut" : "Falcons Central"}`;
+    document
+        .getElementById("viewer")
+        .setAttribute("src", `${toggleModel ? falconsCentralURL : astronaut}`);
+};
