e0a93a51493529ce6f60ea82695b6255fa0e5379
aperez <aperez@falconsdigital.com>
2022-04-06 17:24:43 -0400

    update fullscreen function

--- chrome.js
+++ chrome.js
@@ -1,10 +1,3 @@
-window.addEventListener("load", function () {
-    setTimeout(function () {
-        // Hide the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
-
 const astronautURL =
     "https://modelviewer.dev/shared-assets/models/Astronaut.glb";
 const falconCentralURL = "/models/falcon.glb";
@@ -79,3 +72,37 @@ function generateOverlay(defaultTarget, defaultOrbit, defaultFOV) {
 
     modelViewer.appendChild(overlay);
 }
+
+(function () {
+    var browser = window,
+        doc = browser.document;
+
+    // If there's a hash, or addEventListener is undefined, stop here
+    if (!location.hash || !browser.addEventListener) {
+        //set to 1
+        window.scrollTo(0, 1);
+        var scrollTop = 1,
+            //reset to 0 if needed
+            checkWindowBody = setInterval(function () {
+                if (doc.body) {
+                    clearInterval(checkWindowBody);
+                    scrollTop =
+                        "scrollTop" in doc.body ? doc.body.scrollTop : 1;
+                    browser.scrollTo(0, scrollTop === 1 ? 0 : 1);
+                }
+            }, 15);
+
+        if (browser.addEventListener) {
+            browser.addEventListener(
+                "load",
+                function () {
+                    setTimeout(function () {
+                        //reset to hide address
+                        browser.scrollTo(0, scrollTop === 1 ? 0 : 1);
+                    }, 0);
+                },
+                false
+            );
+        }
+    }
+})();
