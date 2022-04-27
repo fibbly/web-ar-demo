ac6371826c3b40b7c937e954a9f7cb9106ea3244
aperez <aperez@falconsdigital.com>
2022-04-27 10:31:09 -0400

    add annotations to falcon model

--- chrome.js
+++ chrome.js
@@ -5,70 +5,70 @@ const modelViewer = document.querySelector("#modelViewer");
 const switchModelBtn = document.getElementById("switch-model");
 let isFalconsCentral = false;
 
-switchModelBtn.onclick = () => {
-    isFalconsCentral = !isFalconsCentral;
+// switchModelBtn.onclick = () => {
+//     isFalconsCentral = !isFalconsCentral;
 
-    switchModelBtn.classList.toggle("falcon");
+//     switchModelBtn.classList.toggle("falcon");
 
-    switchModelBtn.innerHTML = `${
-        isFalconsCentral ? "Astronaut" : "Falcons Central"
-    }`;
+//     switchModelBtn.innerHTML = `${
+//         isFalconsCentral ? "Astronaut" : "Falcons Central"
+//     }`;
 
-    document.querySelector(".hotspot-1").style.display = isFalconsCentral
-        ? "none"
-        : "display";
+//     document.querySelector(".hotspot-1").style.display = isFalconsCentral
+//         ? "none"
+//         : "display";
 
-    modelViewer.setAttribute(
-        "src",
-        `${isFalconsCentral ? falconCentralURL : astronautURL}`
-    );
-};
+//     modelViewer.setAttribute(
+//         "src",
+//         `${isFalconsCentral ? falconCentralURL : astronautURL}`
+//     );
+// };
 
-const annotationClicked = (annotation) => {
-    let dataset = annotation.dataset;
+// const annotationClicked = (annotation) => {
+//     let dataset = annotation.dataset;
 
-    const defaultTarget = modelViewer.cameraTarget,
-        defaultOrbit = modelViewer.cameraOrbit,
-        defaultFOV = modelViewer.fieldOfView;
+//     const defaultTarget = modelViewer.cameraTarget,
+//         defaultOrbit = modelViewer.cameraOrbit,
+//         defaultFOV = modelViewer.fieldOfView;
 
-    modelViewer.cameraTarget = dataset.target;
-    modelViewer.cameraOrbit = dataset.orbit;
-    modelViewer.fieldOfView = dataset.fov;
+//     modelViewer.cameraTarget = dataset.target;
+//     modelViewer.cameraOrbit = dataset.orbit;
+//     modelViewer.fieldOfView = dataset.fov;
 
-    document.querySelector(".hand").style.display = "block";
-    document.querySelector(".hand-info").style.display = "block";
+//     document.querySelector(".hand").style.display = "block";
+//     document.querySelector(".hand-info").style.display = "block";
 
-    generateOverlay(defaultTarget, defaultOrbit, defaultFOV);
-};
+//     generateOverlay(defaultTarget, defaultOrbit, defaultFOV);
+// };
 
-modelViewer.querySelectorAll("button").forEach((hotspot) => {
-    hotspot.addEventListener("click", () => annotationClicked(hotspot));
-});
+// modelViewer.querySelectorAll("button").forEach((hotspot) => {
+//     hotspot.addEventListener("click", () => annotationClicked(hotspot));
+// });
 
-function generateOverlay(defaultTarget, defaultOrbit, defaultFOV) {
-    if (document.getElementById("overlay")) return;
+// function generateOverlay(defaultTarget, defaultOrbit, defaultFOV) {
+//     if (document.getElementById("overlay")) return;
 
-    const overlay = document.createElement("div");
-    overlay.id = "overlay";
-    overlay.style.position = "absolute";
-    overlay.style.top = 0;
-    overlay.style.left = 0;
-    overlay.style.width = "100vw";
-    overlay.style.height = "100vh";
-    // overlay.style.backgroundColor = "red";
+//     const overlay = document.createElement("div");
+//     overlay.id = "overlay";
+//     overlay.style.position = "absolute";
+//     overlay.style.top = 0;
+//     overlay.style.left = 0;
+//     overlay.style.width = "100vw";
+//     overlay.style.height = "100vh";
+//     // overlay.style.backgroundColor = "red";
 
-    overlay.onclick = () => {
-        console.log("overlay clicked");
+//     overlay.onclick = () => {
+//         console.log("overlay clicked");
 
-        document.querySelector(".hand").style.display = "none";
-        document.querySelector(".hand-info").style.display = "none";
+//         document.querySelector(".hand").style.display = "none";
+//         document.querySelector(".hand-info").style.display = "none";
 
-        modelViewer.cameraTarget = defaultTarget;
-        modelViewer.cameraOrbit = defaultOrbit;
-        modelViewer.fieldOfView = defaultFOV;
+//         modelViewer.cameraTarget = defaultTarget;
+//         modelViewer.cameraOrbit = defaultOrbit;
+//         modelViewer.fieldOfView = defaultFOV;
 
-        overlay.remove();
-    };
+//         overlay.remove();
+//     };
 
-    modelViewer.appendChild(overlay);
-}
+//     modelViewer.appendChild(overlay);
+// }
